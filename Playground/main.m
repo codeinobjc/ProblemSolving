b5486190e5076ad123e7dcf2f5b3c9c660a56eea
Joyce Kumar <codeinobjc@gmail.com>
2019-11-04 16:03:33 -0800

    Initial Commit

---
+++ Playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Playground
+//
+//  Created by Keynes Paul on 11/4/19.
+//  Copyright © 2019 com.codeinobjc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
