559e941302be90d4d2d459ed6ceb4d02050c66c0
Joyce Kumar <codeinobjc@gmail.com>
2019-11-27 14:56:08 -0800

    feat:Problem solving with static arrays - Find a majority element in an array of size ’n’ - finding mode using histogram - O(N) - Find the number occurring odd number of times in a given array of size 'n'- finding mode using histogram - O(N) - Algorithm to reverse an array- exchange first and last converging to the middle.- O(N) - Rotate an array with element count - find pairs of number counting to a sum - find pairs of number counting to a sum - hash map solution

---
+++ Playground/Arrays.h
@@ -0,0 +1,19 @@
+//
+//  Arrays.h
+//  Playground
+//
+//  Created by Keynes Paul on 11/18/19.
+//  Copyright © 2019 com.codeinobjc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Arrays : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+
